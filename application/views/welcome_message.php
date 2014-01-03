d0b19653623a511799d3176d887ec38ffa74b080
marius-miron <marius.costel.miron@gmail.com>
2014-01-03 02:07:46 +0200

    integrare  HomePage cu Twitter Bootstrap

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,13 +3,25 @@
     <head>
         <meta charset="utf-8">
         <title>Soft-Firme</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css">
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
     </head>
     <body>
-
-        <div id="container">
-            <h1>Bine ati venit pe pagina noastra!</h1>
-
-            <div id="body">
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Soft Firme</h1>
+                <h3>Bine ati venit !</h3>
+                <p>
+                    <a class="btn btn-lg btn-success" href="#" role="button">Adauga firma</a>
+                </p>
+            </div>
+            <div class="row">
                 <ul>
                     <li>Altfactor</li>
                     <li>Google</li>
@@ -17,15 +29,15 @@
                     <li>Selir</li>
                     <li>Yahoo</li>
                 </ul>
-                <button type="button">Adauga firma</button>
+            </div>
+            <div class="jumbotron">
                 <form>
-                    <input type="text" />
+                    <input type="text" class="search"/>
                     <input type="submit" value="Cauta"/>
                 </form>
             </div>
-
-            <p class="footer"></p>
         </div>
 
+        <p class="footer"></p>
     </body>
 </html>
